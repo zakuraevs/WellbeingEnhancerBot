1ab689c38c3af963e4bd20312e4ee9431465b04b
Sergey Zakuraev <50769345+zakuraevs@users.noreply.github.com>
2020-09-19 17:46:42 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,4 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+[wb_bot]
+token = <token_here> #actually written on ont he file wheresion whioch is hosted.
 

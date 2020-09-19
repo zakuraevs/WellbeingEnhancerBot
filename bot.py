05579a4ffadbddfb3dffca0dddad0718cb0970fb
Sergey Zakuraev <50769345+zakuraevs@users.noreply.github.com>
2020-09-19 17:46:18 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import configparser as cfg
+
+class wellbeing_bot():
+
+    def __init__(self, config):
+        self.token = self.read_token_from_config_file(settings)
+        self.base = "https://api.telegram.org/bot{}/".format(self.token)
+
+    def get_updates(self, offset=None):
+        url = self.base + "getUpdates?timeout=100"
+        if offset:
+            url = url + "&offset={}".format(offset + 1)
+        r = requests.get(url)
+        return json.loads(r.content)
+
+    def send_message(self, msg, chat_id):
+        url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
+        if msg is not None:
+            requests.get(url)
+
+    def read_token_from_config_file(self, settings):
+        parser = cfg.ConfigParser()
+        parser.read(settings)
+        return parser.get('creds', 'token')
